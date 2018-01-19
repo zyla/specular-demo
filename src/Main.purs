16156a52d10e39c7eebc2221b577d27bd3d5484f
Maciej Bielecki <zyla@prati.pl>
2018-01-19 12:38:36 +0100

    Changes

--- src/Main.purs
+++ src/Main.purs
@@ -2,61 +2,82 @@ module Main where
 
 import Prelude
 
+import Control.Monad.Aff (delay)
+import Control.Monad.Aff.Class (liftAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (log, logShow)
+import Control.Monad.IO (IO(..))
 import Control.Monad.IO.Effect (INFINITY)
 import Control.Monad.IOSync (runIOSync)
 import Data.Monoid (mempty)
+import Data.Time.Duration (Milliseconds(..))
+import Data.Tuple (Tuple(..))
+import Partial.Unsafe (unsafeCrashWith)
 import Specular.Dom.Builder.Class (dynText, el, text)
+import Specular.Dom.Node.Class ((:=))
 import Specular.Dom.Widget (class MonadWidget, runMainWidgetInBody)
 import Specular.Dom.Widgets.Button (buttonOnClick)
-import Specular.Dom.Widgets.Input (textInputOnChange)
-import Specular.FRP (Dynamic, current, dynamic, dynamic_, holdDyn, leftmost, pull, weaken)
-import Specular.FRP.Base (foldDyn, readBehavior)
+import Specular.Dom.Widgets.Input (textInputOnChange, textInputOnInput)
+import Specular.FRP (Dynamic, current, dynamic, dynamic_, fixFRP_, holdDyn, leftmost, pull, tagDyn, weaken)
+import Specular.FRP.Async (RequestState(..), asyncRequest, performEvent)
+import Specular.FRP.Base (foldDyn, readBehavior, subscribeEvent_)
+import Specular.FRP.WeakDynamic (subscribeWeakDyn_)
 
 main :: Eff (infinity :: INFINITY) Unit
 main = runIOSync $ runMainWidgetInBody mainWidget
 
 data What = Button | TextField
 
+validate :: String -> IO Boolean
+validate str = do
+  liftAff $ delay (Milliseconds 1300.0)
+  pure true
+
+save :: String -> IO Unit
+save str = do
+  liftEff $ log $ "Saving: " <> str
+  liftAff $ delay (Milliseconds 1300.0)
+  liftEff $ log $ "Saved."
+
 mainWidget
   :: forall m
    . MonadWidget m
   => m Unit
 mainWidget = do
-  clickedBtn <- buttonOnClick (pure mempty) (text "Button")
-  clickedText <- buttonOnClick (pure mempty) (text "Text field")
+  value <- textInputOnChange "" mempty
 
-  (value :: Dynamic What) <- holdDyn Button $
-    leftmost
-      [ Button    <$ clickedBtn
-      , TextField <$ clickedText
-      ]
+  (validationState :: Dynamic (RequestState Boolean)) <- asyncRequest $ map validate value
 
-  el "br" (pure unit)
+  {-
+  dynText $ weaken $ map
+    (case _ of
+       NotRequested -> ""
+       Loading -> "Loading..."
+       Loaded true -> "valid" 
+       Loaded false -> "invalid") validationState
+       -}
 
-  someOtherValue <- textInputOnChange "foo" mempty
+  fixFRP_ $ \omega -> do
+    let buttonAttrs =
+          map (\e -> if e then mempty else "disabled" := "disabled") omega.enabled
+    submitClicked <- buttonOnClick buttonAttrs $ text "Submit"
 
-  el "br" (pure unit)
 
-  let
-    dynWidget :: Dynamic (m (Dynamic String))
-    dynWidget =
-      (\what -> case what of
+    -- control
 
-         Button -> do
-           void $ buttonOnClick (pure mempty) (text "Btn")
-           pure (pure "It's a button!")
+    let submit = tagDyn value submitClicked
 
-         TextField -> do
-           initial <- pull $ readBehavior $ current someOtherValue
-           textInputOnChange initial mempty
+    saved <- performEvent $ map save submit
 
-      ) <$> value
+    (loading :: Dynamic Boolean) <- holdDyn false $
+      leftmost
+        [ true  <$ submit
+        , false <$ saved
+        ]
 
-  value2 <- map join $ dynamic dynWidget
+    let enabled = map not loading
 
-  el "br" (pure unit)
+    pure {enabled}
 
-  dynText $ weaken value2
-
--- dynamic ∷ ∀ m a. MonadWidget m ⇒ Dynamic (m a) → m (Dynamic a)
+-- asyncRequest :: Dynamic (IO a) -> m (Dynamic (RequestState a))
